14d8826f667d369a3dda7ad72c6c4ecc3b1c4506
anindyoboral <anindyoboral@gmail.com>
2024-02-07 10:46:42 +0100

    initial

---
+++ src/main/java/com/example/sandwichapi/SandwichApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.sandwichapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SandwichApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SandwichApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sandwichapi/SandwichApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sandwichapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SandwichApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
